b06f1d1420dfc5b705e795daa0b3fe3f87958a71
Yo'av Moshe <github@yoavmoshe.com>
2022-06-30 08:24:35 +0000

    fix broken "edit page" link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: 'https://github.com/cloudflare/webcm/docs/edit/main/',
+          editUrl: 'https://github.com/cloudflare/webcm-docs/edit/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
